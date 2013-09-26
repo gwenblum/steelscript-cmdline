8a9489d7022dd14c99abf566809131a71e918259
Chris Kamradt <ckamradt@riverbed.com>
2013-09-26 14:52:25 -0700

    First run at packaging stuffs
    Moved all test files under tests/.  Created setup.py

---
+++ tests/test_shell.py
@@ -0,0 +1,61 @@
+# $Id $ 
+#
+# Copyright 2009-2013 Riverbed Technology, Inc.
+# All Rights Reserved. Confidential.
+
+import pytest
+import logging
+
+from zebra.sshshell import SshShell
+from zebra.cli import Cli
+from zebra.interactive_channel import InteractiveChannel
+
+logging.basicConfig(level=logging.DEBUG)
+
+@pytest.mark.skipif("not config.getvalue('host')")
+def test_shell():
+    logger = logging.getLogger('test_shell')
+    host = pytest.config.getoption('--host')
+    user = pytest.config.getoption('--user')
+    password = pytest.config.getoption('--password')
+    sh = SshShell(host, user, password)
+    sh.connect()
+    (output, exitstatus) = sh.exec_command("pwd")
+    logger.debug("pwd output %s" % output)
+    assert exitstatus == 0
+    sh.disconnect()
+
+@pytest.mark.skipif("not config.getvalue('host')")
+def test_exec_command():
+    """
+    Test exec_command() without doing connect() explicitly first. In this case
+    it will do connect() inside exec_command().
+    """
+    logger = logging.getLogger('test_exec_command')
+    host = pytest.config.getoption('--host')
+    user = pytest.config.getoption('--user')
+    password = pytest.config.getoption('--password')
+    sh = SshShell(host, user, password)
+    (output, exitstatus) = sh.exec_command("pwd")
+    logger.debug("pwd output %s" % output)
+    assert exitstatus == 0
+    sh.disconnect()
+
+@pytest.mark.skipif("not config.getvalue('host')")
+def test_cli():
+    logger = logging.getLogger('test_cli')
+    
+    host = pytest.config.getoption('--host')
+    user = pytest.config.getoption('--user')
+    password = pytest.config.getoption('--password')
+    
+    sh = SshShell(host, user, password)
+    sh.connect()
+    cli = Cli(InteractiveChannel(sh, host))
+    cli.start()
+    cli.enter_level_config()
+    output = cli.run_command("show version concise")
+    logger.debug("version: %s" % output)
+    cli.stop()
+    sh.disconnect()
+    
