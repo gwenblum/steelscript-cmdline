61eea5054cf29f3ca3f64a7ac8a8bb76c64e2389
Jianfeng Tang <jtang@riverbed.com>
2014-01-16 09:06:29 -0800

    Add class to handle ssh transport functionalities.

---
+++ pq_cmdline/sshprocess.py
@@ -0,0 +1,114 @@
+##
+# $Id$
+#
+# Copyright 2013 Riverbed Technology, Inc.
+# All Rights Reserved. Confidential.
+#
+# Basic SSH shell, wrapped around Paramiko
+# Modified based on codes from mgmt-fwk
+
+from __future__ import absolute_import, unicode_literals, print_function,\
+                       division
+
+import paramiko
+import logging
+
+from pq_runtime.exceptions import (re_raise, SshError)
+from pq_cmdline.transport import Transport
+
+
+class SshProcess(Transport):
+    """
+    Ssh transport class to handle ssh connection setup.
+    """
+
+    # Seconds to wait for banner comming out after starting connection.
+    BANNER_TIMEOUT = 5
+
+    def __init__(self, host, user='root', password=''):
+        """
+        Initializer
+
+        :param host: host/ip to ssh into
+        :param user: username to log in with
+        :param password: password to log in with
+        """
+
+        ## Hostname shell connects to
+        self._host = host
+
+        ## Username shell connects with
+        self._user = user
+
+        ## Password shell connects with
+        self._password = password
+
+        ## paramiko.Transport object, the actual SSH engine.
+        # http://www.lag.net/paramiko/docs/
+        self.transport = None
+
+        ## Logging module
+        self._log = logging.getLogger(__name__)
+
+    def connect(self):
+        """
+        Connects to the host.
+
+        :raises SshError: on error
+        """
+        self._log.info('Connecting to "%s" as "%s"' % (self._host, self._user))
+        try:
+            self.transport = paramiko.Transport((self._host, 22))
+            self.transport.banner_timeout = self.BANNER_TIMEOUT
+            self.transport.start_client()
+            self.transport.auth_password(self._user, self._password,
+                                         fallback=False)
+        except paramiko.ssh_exception.SSHException:
+            # Close the session, or the child thread apparently hangs
+            self.disconnect()
+            re_raise(SshError, "Could not connect to %s" % self._host)
+
+    def disconnect(self):
+        """
+        Disconnects from the host
+        """
+
+        if self.transport:
+            self.transport.close()
+
+    def is_connected(self):
+        """
+        Check whether SSH connection is established or not.
+        :return: True if it is connected; returns False otherwise.
+        """
+        if self.transport and self.transport.is_active():
+            return True
+        return False
+
+    def open_interactive_channel(self, term='console', width=80, height=24):
+        """
+        Creates and starts an interactive channel that may be used to
+        communicate with the remote end in a stateful way. This should be used
+        over exec_command whenever the channel must remain open between
+        commands for interactive processing, or when a terminal/tty is
+        necessary; eg, the CLI.
+
+        :param term: terminal type to emulate; defaults to 'console'
+        :param width: width (in characters) of the terminal screen; defaults to 80
+        :param height: height (in characters) of the terminal screen; defaults to 24
+
+        :raises SshError: if the SSH connection has not yet been
+                          established.
+
+        :return: An Paramiko channel that communicate with the remote end in
+                 a stateful way.
+        """
+
+        if (not self.is_connected()):
+            raise SshError('Not connected!')
+
+        channel = self.transport.open_session()
+        channel.get_pty(term, width, height)
+        channel.invoke_shell()
+        channel.set_combine_stderr(True)
+        return channel
---
+++ tests/test_sshprocess.py
@@ -0,0 +1,66 @@
+# $Id $
+#
+# Copyright 2009-2013 Riverbed Technology, Inc.
+# All Rights Reserved. Confidential.
+
+import pytest
+import time
+import select
+from mock import Mock, patch
+
+from pq_runtime.exceptions import SshError
+from pq_cmdline.sshprocess import SshProcess
+
+ANY_HOST = 'host1'
+ANY_USER = 'user1'
+ANY_PASSWORD = 'password1'
+
+
+@pytest.fixture
+def any_sshprocess():
+    return SshProcess(ANY_HOST, ANY_USER, ANY_PASSWORD)
+
+
+def test_members_initialized_correctly(any_sshprocess):
+    assert any_sshprocess._host == ANY_HOST
+    assert any_sshprocess._user == ANY_USER
+    assert any_sshprocess._password == ANY_PASSWORD
+    assert any_sshprocess.transport is None
+
+
+def test_connect(any_sshprocess):
+    with patch('pq_cmdline.sshprocess.paramiko.Transport') as mock:
+        mock_transport = mock.return_value
+        any_sshprocess.connect()
+        assert any_sshprocess.transport == mock_transport
+        assert any_sshprocess.transport.start_client.called
+        assert any_sshprocess.transport.auth_password.called
+
+
+def test_disconnect_closes_transport(any_sshprocess):
+    any_sshprocess.transport = Mock()
+    any_sshprocess.disconnect()
+    assert any_sshprocess.transport.close.called
+
+
+def test_is_connected_if_transport_is_active(any_sshprocess):
+    any_sshprocess.transport = Mock()
+    any_sshprocess.transport.is_active.return_value = True
+    assert any_sshprocess.is_connected()
+
+
+def test_open_interactive_channel_raise_if_not_connected(any_sshprocess):
+    with pytest.raises(SshError):
+        any_sshprocess.open_interactive_channel()
+
+
+def test_open_interactive_channel_if_connected(any_sshprocess):
+    any_sshprocess.is_connected = Mock(name='method')
+    any_sshprocess.is_connected.return_value = True
+    any_sshprocess.transport = Mock()
+    mock_channel = Mock()
+    any_sshprocess.transport.open_session.return_value = mock_channel
+    any_sshprocess.open_interactive_channel()
+    assert any_sshprocess.transport.open_session.called
+    assert mock_channel.get_pty.called
+    assert mock_channel.invoke_shell.called
