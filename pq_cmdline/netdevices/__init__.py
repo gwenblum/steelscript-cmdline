669316bdbc7b311bd718c8ae7352f37b657208e6
Vaibhav Tandon <vtandon@chert.lab.nbttech.com>
2014-04-15 14:25:05 -0700

    pq_cmdline: Added base CLI class and corresponding unit tests
    DesignID: 96505
    ReviewID: 97050
    CC:
    Reviewed-By: handrews, jheimann
    Approved-By: handrews, jheimann

---
+++ pq_cmdline/netdevices/__init__.py
@@ -0,0 +1,210 @@
+# Copyright 2013 Riverbed Technology, Inc.
+# All Rights Reserved. Confidential.
+
+from __future__ import (absolute_import, unicode_literals, print_function,
+                        division)
+
+import logging
+
+from pq_cmdline.ssh_channel import SSHChannel
+from pq_cmdline.sshprocess import SSHProcess
+from pq_cmdline.telnet_channel import TelnetChannel
+from pq_cmdline import exceptions
+
+# Control-u clears any entered text.  Neat.
+DELETE_LINE = b'\x15'
+
+# Command terminator
+ENTER_LINE = b'\r'
+
+
+class CLIMode(object):
+
+    """
+    Different config modes the CLI can be in
+    """
+    SHELL = 'shell'
+    NORMAL = 'normal'
+    ENABLE = 'enable'
+    CONFIG = 'configure'
+    SUBIF = 'subif'
+
+
+class CLI(object):
+
+    """
+    Base class CLI implementation for Network Devices
+    Vendor specific CLI's can inherit from this base class. This class by
+    itself will try to work on a generic CLI if vendor specific class is not
+    present.
+    """
+
+    # Presently the CLI_START_PROMPT is a generic regex which will try to
+    # match prompt patterns, without having a check for hostname
+    # there is a possiblity there might be false positives
+
+    # catch-all regex for generic CLI
+    CLI_START_PROMPT = '(^|\n|\r)(\S+)(#|\$|>|~)(\s)?$'
+    CLI_ANY_PROMPT = CLI_START_PROMPT
+
+    def __init__(self, host, user='admin', password='', terminal='console',
+                 transport_type='ssh'):
+        """
+        Create a new Cli Channel object.
+
+        :param host: host/ip
+        :type host: string
+        :param user: username to log in with
+        :type user: string
+        :param password: password to log in with
+        :type password: string
+        :param terminal:  terminal emulation to use; default to 'console'
+        :type terminal: string
+        :param transport_type: telnet or ssh, defaults to ssh
+        :type transport_type: string
+        """
+
+        self._host = host
+        self._user = user
+        self._password = password
+        self._terminal = terminal
+        self._transport_type = transport_type
+        self._new_transport = False
+        self._transport = None
+        self._log = logging.getLogger(__name__)
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if self._new_transport and self._transport:
+            self._transport.disconnect()
+            self._transport = None
+
+    def start(self):
+        """
+        Initialize underlying channel.
+        """
+        if self._transport_type == 'ssh':
+            self._initialize_cli_over_ssh()
+        elif self._transport_type == 'telnet':
+            self._initialize_cli_over_telnet()
+        else:
+            raise NotImplementedError(
+                "Unsupported transport type %s" % self._transport_type)
+
+    def _initialize_cli_over_ssh(self):
+        """
+        Initialize underlying ssh transport and ssh channel. It expect ssh to
+        shell or cli. Start cli if ssh-ed to shell.
+        """
+
+        # Create SSHProcess
+        if self._transport is None:
+            self._transport = SSHProcess(self._host,
+                                         self._user,
+                                         self._password)
+            self._new_transport = True
+
+        # Create ssh channel and start channel
+        self.channel = SSHChannel(self._transport, self._terminal)
+
+        # Wait for a prompt, try and figure out where we are.  It's a new
+        # channel so we should only be at bash or the main CLI prompt.
+        self.channel.expect([self.CLI_START_PROMPT])
+
+    def _initialize_cli_over_telnet(self):
+        """
+        Create and inititalize telnet channel. It assume telnet to either
+        shell or cli. Start cli if telneted to shell.
+        """
+
+        # Create telnet channel
+        self.channel = TelnetChannel(self._host, self._user, self._password)
+
+        # Start and Wait for a prompt, try and figure out where we are.
+        self.channel.start([self.CLI_START_PROMPT])
+
+    def _send_and_wait(self, text_to_send, match_res, timeout=60):
+        """
+        Discard old data in buffer, sends data, then blocks until some text is
+        received that matches one or more patterns.
+
+        :param text_to_send: Text to send, may be empty.  Note, you are
+                             responsible for your own command terminator!
+        :param match_res: Pattern(s) to look for to be considered successful.
+        :param timeout: Maximum time, in seconds, to wait for a regular
+                        expression match. 0 to wait forever.
+
+        :return: (output, re.MatchObject) where output is the output of the
+                 command (without the matched text), and MatchObject is
+                 a Python re.MatchObject containing data on what was matched.
+        """
+        self.channel.receive_all()
+        self.channel.send(text_to_send)
+        return self.channel.expect(match_res, timeout)
+
+    def _send_line_and_wait(self, text_to_send, match_res, timeout=60):
+        """
+        Same to _send_and_wait but automatically append a newline to data
+        before send.
+
+        :param text_to_send: Text to send, may be empty.  Note, you are
+                             responsible for your own command terminator!
+        :param match_res: Pattern(s) to look for to be considered successful.
+        :param timeout: Maximum time, in seconds, to wait for a regular
+                        expression match. 0 to wait forever.
+
+        :return: (output, re.MatchObject) where output is the output of the
+                 command (without the matched text), and MatchObject is
+                 a Python re.MatchObject containing data on what was matched.
+        """
+        text_to_send = text_to_send + ENTER_LINE
+        return self._send_and_wait(text_to_send, match_res, timeout)
+
+    def exec_command(self, command, timeout=60, output_expected=None):
+        """
+        Executes the given command.
+
+        This method handles detecting simple boolean conditions such as
+        the presence of output or errors.
+
+        :param command:  command to execute, newline appended automatically
+        :param timeout:  maximum time, in seconds, to wait for the command to
+            finish. 0 to wait forever.
+        :param output_expected: If not None, indicates whether output is
+            expected (True) or no output is expected (False).
+            If the opposite occurs, raise UnexpectedOutput. Default is None.
+        :type output_expected: bool or None
+
+        :raises TypeError: if output_expected type is incorrect
+        :raises CmdlineTimeout: on timeout
+        :raises UnexpectedOutput: if output occurs when no output was
+            expected, or no output occurs when output was expected
+
+        :return: output of the command, minus the command itself.
+        """
+
+        if output_expected is not None and not isinstance(
+                output_expected, bool):
+            raise TypeError("exec_command: output_expected requires a boolean "
+                            "value or None")
+
+        self._log.debug('Executing cmd "%s"' % command)
+
+        (output, match) = self._send_line_and_wait(command,
+                                                   self.CLI_ANY_PROMPT,
+                                                   timeout=timeout)
+
+        # CLI adds on escape chars and such sometimes (see bug 75081), so to
+        # remove the command we just send from the output, split the output
+        # into lines, then rejoin it with the first line removed.
+        output = '\n'.join(output.splitlines()[1:])
+
+        if ((output_expected is not None) and (bool(output) !=
+                                               bool(output_expected))):
+            raise exceptions.UnexpectedOutput(command=command,
+                                              output=output,
+                                              expected_output=output_expected)
+        return output
---
+++ tests/netdevices/test_cli.py
@@ -0,0 +1,133 @@
+# Copyright 2009-2013 Riverbed Technology, Inc.
+# All Rights Reserved. Confidential.
+
+from __future__ import (absolute_import, unicode_literals, print_function,
+                        division)
+
+import pytest
+from mock import Mock, MagicMock
+
+from pq_cmdline.netdevices import CLI
+from pq_cmdline import exceptions
+
+ANY_HOST = 'sh1'
+ANY_USER = 'user1'
+ANY_PASSWORD = 'password1'
+ANY_TERMINAL = 'console'
+TRANSPORT_SSH = 'ssh'
+TRANSPORT_TELNET = 'telnet'
+TRANSPORT_UNKNOWN = 'any unknown type'
+ANY_COMMAND = 'show date'
+ANY_COMMAND_OUTPUT = 'Thu Sep 12 19:50:51 GMT 2013'
+ANY_COMMAND_OUTPUT_DATA = '%s\n%s' % (ANY_COMMAND, ANY_COMMAND_OUTPUT)
+ANY_ROOT_COMMAND = 'show'
+ANY_UNKNOWN_LEVEL = 'unknown'
+ANY_TIMEOUT = 120
+
+
+@pytest.fixture
+def any_cli():
+    cli = CLI(ANY_HOST, ANY_USER, ANY_PASSWORD, ANY_TERMINAL, TRANSPORT_SSH)
+    cli.channel = Mock()
+    return cli
+
+
+@pytest.fixture
+def cli_mock_output(any_cli):
+    any_cli._send_line_and_wait = MagicMock(name='method')
+    any_cli._send_and_wait = MagicMock(name='method')
+    return any_cli
+
+
+@pytest.fixture
+def prompt_match():
+    fake_match = MagicMock()
+    fake_match.re = MagicMock()
+    fake_match.re.pattern = CLI.CLI_START_PROMPT
+    return fake_match
+
+
+def test_members_intialize_correctly(any_cli):
+    assert any_cli._host == ANY_HOST
+    assert any_cli._user == ANY_USER
+    assert any_cli._password == ANY_PASSWORD
+    assert any_cli._terminal == ANY_TERMINAL
+    assert any_cli._transport_type == TRANSPORT_SSH
+
+
+def test_start_raises_for_unknown_transport(any_cli):
+    any_cli._transport_type = TRANSPORT_UNKNOWN
+    with pytest.raises(NotImplementedError):
+        any_cli.start()
+
+
+def test_start_initialize_telnet(any_cli):
+    any_cli._transport_type = TRANSPORT_TELNET
+    any_cli._initialize_cli_over_telnet = MagicMock(name='method')
+    any_cli.start()
+    assert any_cli._initialize_cli_over_telnet.called
+
+
+def test_start_initialize_ssh(any_cli):
+    any_cli._initialize_cli_over_ssh = MagicMock(name='method')
+    any_cli.start()
+    assert any_cli._initialize_cli_over_ssh.called
+
+
+def test_context_manger_enter_calls_start(any_cli):
+    any_cli.start = MagicMock(name='method')
+    with any_cli:
+        assert any_cli.start.called
+
+
+def test_use_context_manger_twice_works(any_cli):
+    any_cli.start = MagicMock(name='method')
+    with any_cli:
+        pass
+    with any_cli:
+        pass
+    assert any_cli.start.call_count == 2
+
+
+def test_context_manger_exit_close_transport(any_cli):
+    any_cli.start = MagicMock(name='method')
+    any_cli._new_transport = True
+    mock_transport = Mock()
+    any_cli._transport = mock_transport
+    with any_cli:
+        pass
+    assert mock_transport.disconnect.called
+    assert any_cli._transport is None
+
+
+def test_context_manger_exit_if_not_new_transport(any_cli):
+    any_cli.start = MagicMock(name='method')
+    any_cli._new_transport = False
+    mock_transport = Mock()
+    any_cli._transport = mock_transport
+    with any_cli:
+        pass
+    assert not mock_transport.disconnect.called
+    assert any_cli._transport == mock_transport
+
+
+def test_exec_command_output(cli_mock_output, prompt_match):
+    cmo = cli_mock_output
+    cmo._send_line_and_wait.return_value = (ANY_COMMAND_OUTPUT_DATA,
+                                            prompt_match)
+
+    assert cmo.exec_command(ANY_COMMAND) == ANY_COMMAND_OUTPUT
+    assert cmo.exec_command(ANY_COMMAND,
+                            output_expected=True) == ANY_COMMAND_OUTPUT
+    with pytest.raises(exceptions.UnexpectedOutput):
+        cmo.exec_command(ANY_COMMAND, output_expected=False)
+
+
+def test_exec_command_no_output(cli_mock_output, prompt_match):
+    cmo = cli_mock_output
+    cmo._send_line_and_wait.return_value = ('%s\n' % ANY_COMMAND,
+                                            prompt_match)
+    assert cmo.exec_command(ANY_COMMAND) == ''
+    assert cmo.exec_command(ANY_COMMAND, output_expected=False) == ''
+    with pytest.raises(exceptions.UnexpectedOutput):
+        cmo.exec_command(ANY_COMMAND, output_expected=True)
