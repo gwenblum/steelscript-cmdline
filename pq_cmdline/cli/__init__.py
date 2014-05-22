31d3719528daba7a131ecb793b246a883e9e9aa8
lsouza <lsouza@riverbed.com>
2014-05-22 05:41:30 -0700

    Fixing issue where receive_all() was blocking endlessly

--- pq_cmdline/cli/__init__.py
+++ pq_cmdline/cli/__init__.py
@@ -163,7 +163,10 @@ class CLI(object):
                  command (without the matched text), and MatchObject is
                  a Python re.MatchObject containing data on what was matched.
         """
-        self.channel.receive_all()
+        # TODO: There was a call to 'self.channel.receive_all()' here, which
+        # appears to be used to 'flush' the buffer first.  For interactive
+        # prompts on libvirtchannel, this was causing an endless blocking call.
+        # We still probably want to figure out an alternative.
         self.channel.send(text_to_send)
         return self.channel.expect(match_res, timeout)
 
