f08612c628368af55780657b2df6a40e561dec8f
wenxuan guo <wguo@riverbed.com>
2014-10-20 11:07:27 -0400

    address some minor issues regarding comments

--- examples/system_summary.py
+++ examples/system_summary.py
@@ -1,6 +1,10 @@
-#!/usr/bin/python
-# Copyright 2009-2013 Riverbed Technology, Inc.
-# All Rights Reserved. Confidential.
+# Copyright (c) 2014 Riverbed Technology, Inc.
+#
+# This software is licensed under the terms and conditions of the MIT License
+# accompanying the software ("License").  This software is distributed "AS IS"
+# as set forth in the License.
+
+
 from __future__ import (absolute_import, unicode_literals, print_function,
                         division)
 
@@ -9,24 +13,24 @@ from steelscript.cmdline import cli
 
 
 def filter(input_string, match=None, unmatch=None):
-    """filter input_string on a per-line basis
+    """Filter input_string on a per-line basis
 
     :param input_string: the input string to be filtered
     :param match: a string required in one line
     :param unmatch: a string should not exist in one line
     """
     ret = []
-    for ln in input_string.split('\n'):
-        if ((match is None or match in ln) and
-                (unmatch is None or unmatch not in ln)):
-            ret.append(ln)
+    for line in input_string.split('\n'):
+        if ((match is None or match in line) and
+                (unmatch is None or unmatch not in line)):
+            ret.append(line)
     return '\n'.join(ret)
 
 
 class BasicInfoCLI(cli.CLI):
 
     def disk_usage(self):
-        """parse the output of 'df -h' and return a list of 3-element lists
+        """Parse the output of 'df -h' and return a list of 3-element lists
         each list consist of mount point, use percentage and total size
 
         :param input_string: the output of df -h
@@ -34,19 +38,19 @@ class BasicInfoCLI(cli.CLI):
         output = filter(self.exec_command('df -h\n'), match='%',
                         unmatch='Filesystem')
         ret = []
-        for ln in output.split('\n'):
-            fs = ln.split()
+        for line in output.split('\n'):
+            fs = line.split()
             # fs follows pattern as "/dev/sda3 862G  183G  637G  23% /"
             # as "filesystem total used available percentage mount-point"
             ret.append(' '.join([fs[-1], fs[-5], fs[-2]]))
         return '\n'.join(ret)
 
     def time_info(self):
-        """return time/timezone info by running 'date' command"""
+        """Return time/timezone info by running 'date' command"""
         return self.exec_command('date\n')
 
     def cpu_load(self):
-        """return the cpu load for the last minute, 5 minutes and 15 minutes"""
+        """Return the cpu load for the last minute, 5 minutes and 15 minutes"""
         # uptime returns as
         # 16:45  up 11 days,  6:49, 8 users, load averages: 2.00 1.75 1.67
         return ' '.join(self.exec_command('uptime\n').split(' ')[-5:])
@@ -78,9 +82,9 @@ class BasicInfoApp(Application):
         with BasicInfoCLI(hostname=self.options.host,
                           username=self.options.username,
                           password=self.options.password) as cli:
-            print (cli.time_info())
-            print (cli.cpu_load())
-            print (cli.disk_usage())
+            print(cli.time_info())
+            print(cli.cpu_load())
+            print(cli.disk_usage())
 
 
 if __name__ == '__main__':
