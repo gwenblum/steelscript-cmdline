c0dae58cfca39c9039ca42177bd95e42811d236f
wenxuan guo <wguo@riverbed.com>
2014-12-22 12:13:46 -0500

    Add ipaddress to setup.py; delete setup.cfg to avoid searching pypibox

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
     install_requires=['paramiko',
                       'scp',
                       'steelscript',
+                      'ipaddress'
                       ],
     extras_require={'test': test,
                     'doc': doc,
